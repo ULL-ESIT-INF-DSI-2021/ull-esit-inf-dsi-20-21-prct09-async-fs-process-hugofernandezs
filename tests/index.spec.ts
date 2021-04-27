cee7c029a4addb4a677b2a98af8d4ca775a84a5f
Hugo Fernández Solís <alu0101112664@ull.edu.es>
2021-04-27 13:23:33 +0100

    Actualización de index

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,9 +1,10 @@
 import 'mocha';
 import {expect} from 'chai';
+import {add} from '../src/index';
 
 describe('Test vacío:', () => {
   it('Funcionamiento del test vacío', () => {
-    expect(true).to.be.equal(true);
+    expect(add(2, 3)).to.be.equal(5);
   });
 },
 );
